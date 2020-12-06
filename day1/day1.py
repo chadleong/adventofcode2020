b55f811e1eed85755d25c58a27d0f218ad624c3b
Arbitel <chewyeong90@gmail.com>
2020-12-06 15:26:07 +0100

    day 1 completed

--- day1/day1.py
+++ day1/day1.py
@@ -6,7 +6,8 @@ with open(dir+'\\inputs.txt', 'r', newline=None, encoding='utf-8') as f:
     l=[i.strip() for i in f]
     f.close()
 
-def find_2020(arg):
+#part 1
+def find_2020_part1(arg):
     for i in range(len(arg)):
         first=int(arg[i])
         for j in range(i+1,len(arg)):
@@ -15,5 +16,16 @@ def find_2020(arg):
                     print(first, int(arg[j+1]))
                     print(first*int(arg[j+1]))
 
+#part 2
+def find_2020_part2(arg):
+    for i in range(len(arg)):
+        first=int(arg[i])
+        for j in range(len(arg)):
+            if j<199:
+                second=int(arg[j])
+                for k in range(len(arg)):
+                    if first+second+int(arg[k])==2020:
+                        print(first, second, int(arg[k]))
+                        print(first*second*int(arg[k]))
 
-find_2020(l)
+find_2020_part2(l)
